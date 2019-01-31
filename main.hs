a929411558f2e061b258d1de57ddc0d65fac93c1
Meghana Ganapathiraju <meghanag@live.unc.edu>
2019-01-31 17:55:17 +0000

    t2

--- main.hs
+++ main.hs
@@ -1,2 +1,3 @@
-finalgrade :: [a] -> [a]-> a
-finalgrade x y  = ((take 1 x)*(take 1 y) +(finalgrade (drop 1 x) (drop 1 y))/(length x)
+finalgrade :: (Num a) =>[a] -> [a]-> a
+finalgrade  x y = (head x)*head(y) +(finalgrade (drop 1 x) (drop 1 y))
+
